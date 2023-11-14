156d45e8f70e8ed451f04405ae9fd4bc2e8a3539
h0x0er <jatink843@protonmail.com>
2023-11-14 15:52:06 +0530

    decoding kernel time to userspace time

--- user/event/log_fmt.go
+++ user/event/log_fmt.go
@@ -50,7 +50,14 @@ func LogGoTLSEvent(payloadType uint8, exe []byte, timestamp uint64, data []byte,
 func LogString(exe []byte, timestamp uint64, data []byte) string {
 	logFmt := new(LogFmt)
 	logFmt.Executable = unix.ByteSliceToString(exe)
-	logFmt.Timestamp = timestamp
+
+	t, err := DecodeKtime(int64(timestamp), false)
+	if err == nil {
+		logFmt.Timestamp = uint64(t.Unix())
+	} else {
+		logFmt.Timestamp = timestamp
+
+	}
 
 	var req *http.Request = nil
 
@@ -98,4 +105,20 @@ func writeLog(message string) {
 	location, _ := time.LoadLocation("Etc/GMT")
 	f.WriteString(fmt.Sprintf("%s:%s\n", time.Now().In(location).Format("Mon, 02 Jan 2006 15:04:05 MST"), message))
 
+}
+
+func DecodeKtime(ktime int64, monotonic bool) (time.Time, error) {
+	var clk int32
+	if monotonic {
+		clk = int32(unix.CLOCK_MONOTONIC)
+	} else {
+		clk = int32(unix.CLOCK_BOOTTIME)
+	}
+	currentTime := unix.Timespec{}
+	if err := unix.ClockGettime(clk, &currentTime); err != nil {
+		return time.Time{}, err
+	}
+	diff := ktime - currentTime.Nano()
+	t := time.Now().Add(time.Duration(diff))
+	return t, nil
 }
