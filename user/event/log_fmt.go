708310dbb47491b6740c3cfcf9d9b4cdab840d6c
h0x0er <jatink843@protonmail.com>
2023-11-30 17:05:37 +0530

    fix

--- user/event/log_fmt.go
+++ user/event/log_fmt.go
@@ -52,8 +52,6 @@ func LogString(exe []byte, timestamp uint64, data []byte) string {
 	logFmt.Executable = unix.ByteSliceToString(exe)
 	logFmt.Timestamp = timestamp
 
-	}
-
 	var req *http.Request = nil
 
 	frame, err := http2util.BytesToFrame(data)
@@ -100,20 +98,4 @@ func writeLog(message string) {
 	location, _ := time.LoadLocation("Etc/GMT")
 	f.WriteString(fmt.Sprintf("%s:%s\n", time.Now().In(location).Format("Mon, 02 Jan 2006 15:04:05 MST"), message))
 
-}
-
-func DecodeKtime(ktime int64, monotonic bool) (time.Time, error) {
-	var clk int32
-	if monotonic {
-		clk = int32(unix.CLOCK_MONOTONIC)
-	} else {
-		clk = int32(unix.CLOCK_BOOTTIME)
-	}
-	currentTime := unix.Timespec{}
-	if err := unix.ClockGettime(clk, &currentTime); err != nil {
-		return time.Time{}, err
-	}
-	diff := ktime - currentTime.Nano()
-	t := time.Now().Add(time.Duration(diff))
-	return t, nil
 }
